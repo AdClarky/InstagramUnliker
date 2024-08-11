d45396cc616ce2f75bb3d4961fe7a90bd30dc05c
Toby <tobyeclark@gmail.com>
2024-08-11 22:06:40 +0100

    Fixed histroy

--- history.py
+++ history.py
@@ -3,21 +3,19 @@ from typing import List
 
 
 def write(url: str) -> None:
-    with open("history.csv", "w", newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([url])
+    with open("history.csv", "a", newline='') as f:
+        f.write(url + ",")
 
 
 def read() -> List[str]:
     try:
         with open("history.csv", "r") as f:
             reader = csv.reader(f)
-            return list(reader)[0]
+            return list(reader)[0][:-1]
     except FileNotFoundError:
         return []
 
 
 def write_empty(url: str) -> None:
-    with open("errors.csv", "w", newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([url])
+    with open("errors.csv", "a", newline='') as f:
+        f.write(url + ",")
