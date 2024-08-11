4e6d450058a3a631e43df25a3ed22139f8496581
Toby <tobyeclark@gmail.com>
2024-08-11 22:08:40 +0100

    Added some logging and it now saves and skips already processed items

--- unliker.py
+++ unliker.py
@@ -4,6 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+import history
 from urlReader import read_urls
 from config import Config
 
@@ -11,9 +12,11 @@ from config import Config
 def unlike_all(driver: webdriver.Firefox):
     elements = driver.find_elements(By.CSS_SELECTOR, "[aria-label=Unlike]")
     if not len(elements):
-        logging.warning(f"[WARNING] No unlike found for {driver.current_url}")
+        logging.warning(f"No unlike found for {driver.current_url}")
+        history.write_empty(driver.current_url)
     for element in elements:
         element.click()
+    history.write(driver.current_url)
 
 
 def main():
@@ -25,6 +28,10 @@ def main():
 
     config = Config("config.json")
 
+    done_urls = history.read()
+
+    print(done_urls)
+
     urls = read_urls(config.liked_posts_path)
 
     options = webdriver.FirefoxOptions()
@@ -33,11 +40,16 @@ def main():
     driver = webdriver.Firefox(options=options)
 
     for url in urls:
+        if url in done_urls:
+            logging.info(f"Skipping as already processed: {url}")
+            continue
         logging.info(f"Processing {url}")
         driver.get(url)
         time.sleep(10)
         unlike_all(driver)
         time.sleep(10)
+
+    driver.quit()
 
 
 if __name__ == '__main__':
