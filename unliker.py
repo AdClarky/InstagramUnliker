8fdc09c5345fcba33f1e40d0f145338c7f406afb
Toby <tobyeclark@gmail.com>
2024-08-11 21:20:19 +0100

    Added logging

--- unliker.py
+++ unliker.py
@@ -1,3 +1,4 @@
+import logging
 import time
 
 from selenium import webdriver
@@ -9,11 +10,19 @@ from config import Config
 
 def unlike_all(driver: webdriver.Firefox):
     elements = driver.find_elements(By.CSS_SELECTOR, "[aria-label=Unlike]")
+    if not len(elements):
+        logging.warning(f"[WARNING] No unlike found for {driver.current_url}")
     for element in elements:
         element.click()
 
 
 def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s] %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
     config = Config("config.json")
 
     urls = read_urls(config.liked_posts_path)
@@ -24,6 +33,7 @@ def main():
     driver = webdriver.Firefox(options=options)
 
     for url in urls:
+        logging.info(f"Processing {url}")
         driver.get(url)
         time.sleep(10)
         unlike_all(driver)
