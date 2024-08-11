72d5ce22f54147040f39218fc88bb79c2112b078
Toby <tobyeclark@gmail.com>
2024-08-11 21:37:35 +0100

    Made history just funcs

--- history.py
+++ history.py
@@ -2,13 +2,19 @@ import csv
 from typing import List
 
 
-class History:
-    def write(self, url: str) -> None:
-        with open("history.csv", "w", newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow([url])
+def write(url: str) -> None:
+    with open("history.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([url])
 
-    def read(self) -> List[str]:
-        with open("history.csv", "r") as f:
-            reader = csv.reader(f)
-            return list(reader)[0]
+
+def read() -> List[str]:
+    with open("history.csv", "r") as f:
+        reader = csv.reader(f)
+        return list(reader)[0]
+
+
+def write_empty(url: str) -> None:
+    with open("errors.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([url])
